5bebdbcee6d4e79636fbba0c5243091fdf8bfd41
junseok5 <vkehrkrj@naver.com>
2019-03-12 14:12:06 +0900

    Previewboard deleted => Test Completed

--- src/routers/v1.0/boardRouter.ts
+++ src/routers/v1.0/boardRouter.ts
@@ -6,6 +6,6 @@ const router: Router = new Router()
 router.get("/boards", boardsCtrl.listBoard)
 router.get("/boards/search/preview", boardsCtrl.listPreview)
 router.post("/boards", boardsCtrl.wrtieBoard)
-router.patch("/boards:id", boardsCtrl.updateBoard)
+router.patch("/boards/:id", boardsCtrl.updateBoard)
 
 export default router
